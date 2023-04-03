9fa287244632aae0d1c3e0a38cd23cb6d10c4e15
Elizabeth Hunt <elizabeth.hunt@simponic.xyz>
2023-04-02 21:06:25 -0600

    Add timer module files

--- server/src/timer/timer.module.ts
+++ server/src/timer/timer.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TimerGateway } from './timer.gateway';
+import { TimerController } from './timer.controller';
+import { TimerService } from './timer.service';
 
-@Module({})
+@Module({
+  providers: [TimerGateway, TimerService],
+  controllers: [TimerController]
+})
 export class TimerModule {}
